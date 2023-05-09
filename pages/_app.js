a52b00415a8b2d96511640fe91195c3e1382b1a0
Sang jin Lee <w1004123@gmail.com>
2023-05-10 00:28:30 +0900

    GQL useMutation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  const client = new ApolloClient({
+    uri: "http://practice.codebootcamp.co.kr/graphql",
+    cache : new InMemoryCache() //컴퓨터의 메모리나 디스크에 저장하겠다. //나중에 할것
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
---
+++ pages/rest-get/gpl/index.js
@@ -0,0 +1,26 @@
+import { gql,useMutation } from '@apollo/client';
+
+import React from 'react'
+const CREATE_BOARD = gql`
+  mutation{
+    createBoard(writer:"이상진", title:"BSSM", contents: "2112"){
+      _id
+      number
+      message
+    }
+  }
+`
+
+export default function index() {
+  const [나의함수] = useMutation(CREATE_BOARD)
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수()
+    console.log(result)
+  }
+  return (
+    <>
+      <button onClick={onClickSubmit}>GRAPH-API(동기) 요청하기</button>
+    </>
+  )
+}
